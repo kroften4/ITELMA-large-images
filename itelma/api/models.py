953ce3135ad88d856466a1c79862f2d029f42ae3
kroften4 <134956188+kroften4@users.noreply.github.com>
2024-11-22 21:27:54 +0300

    Add api/list/ endpoint

--- itelma/api/models.py
+++ itelma/api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Image(models.Model):
-    name = models.CharField(primary_key=True)
+    name = models.CharField(primary_key=True, max_length=100)
     last_called = models.DateTimeField()
--- itelma/api/views.py
+++ itelma/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Image
+from rest_framework import viewsets
+from .serializers import ImageSerializer
 
-# Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all().order_by('-last_called')
+    serializer_class = ImageSerializer
