c6bb20ae880beb603a5e3128969a27158edf6131
kroften4 <134956188+kroften4@users.noreply.github.com>
2024-11-23 19:15:52 +0300

    Update views.py

--- itelma/api/views.py
+++ itelma/api/views.py
@@ -1,30 +1,9 @@
-from .models import Original
 from rest_framework import viewsets
 
 from .serializers import ImageSerializer
-from pathlib import Path
-from django.http import HttpResponse
 from api.models import Original
-from math import ceil
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Original.objects.all().order_by('-last_opened')
     serializer_class = ImageSerializer
-
-
-def get_preview(request, img_name):
-    preview_image = open(Path(IMAGES_DIR, img_name, "preview.png"), 'rb')
-    return HttpResponse(preview_image, content_type="image/png")
-
-
-# def get_tile(request, img_name):
-#     scale = request.GET.get('scale')
-#     x = int(request.GET.get('x'))
-#     y = int(request.GET.get('y'))
-#     img = Image.objects.get(pk=img_name)
-#     width = img.width
-#     size_x = ceil(width / 2000)
-#     tile_id = y * size_x + x
-#     preview_image = open(Path(IMAGES_DIR, img_name, f"tiles_{scale}", f"t{tile_id}.png"), 'rb')
-#     return HttpResponse(preview_image, content_type="image/png")
