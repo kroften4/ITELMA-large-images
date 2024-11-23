d5e92be7e0928daef63b783362d15fb01f57a6dc
kroften4 <134956188+kroften4@users.noreply.github.com>
2024-11-23 22:50:01 +0300

    Fix models names

--- itelma/api/views.py
+++ itelma/api/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, renderer_classes
 from .serializers import OriginalSerializer, PreviewSerializer, ScaleTileSerializer
 from api.models import Original, Preview, ScaleTile
+from rest_framework.renderers import JSONRenderer
 
 
 class ImageViewSet(viewsets.ModelViewSet):
@@ -44,15 +46,19 @@ class ImageViewSet(viewsets.ModelViewSet):
 #         return Response(qs_serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(('GET',))
+@renderer_classes((JSONRenderer,))
 def get_preview(request, img_name):
-    qs = Preview.objects.get(name=img_name)
+    qs = Preview.objects.get(original__name=img_name)
     qs_serializer = PreviewSerializer(qs)
     return Response(qs_serializer.data, status=status.HTTP_200_OK)
 
 
+@api_view(('GET',))
+@renderer_classes((JSONRenderer,))
 def get_tile(request, img_name):
     x = request.GET.get('x')
     y = request.GET.get('y')
-    qs = ScaleTile.objects.get(name=img_name, x=x, y=y)
+    qs = ScaleTile.objects.get(original__name=img_name, x=x, y=y)
     qs_serializer = ScaleTileSerializer(qs)
     return Response(qs_serializer.data, status=status.HTTP_200_OK)
