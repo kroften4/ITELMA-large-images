4a9d7208e3a6c042f29bcf22905e1f7bfd2bc0a5
kroften4 <134956188+kroften4@users.noreply.github.com>
2024-11-23 18:58:31 +0300

    Fix models

--- itelma/api/admin.py
+++ itelma/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Original
+from .models import Original, ScaleTile, Preview
 
 # Register your models here.
 
 admin.site.register(Original)
+admin.site.register(ScaleTile)
+admin.site.register(Preview)
--- itelma/api/models.py
+++ itelma/api/models.py
@@ -14,18 +14,17 @@ class Original(models.Model):
 
 def preview_upload_path(instance, filename):
     extension = filename.split('.')[-1]
-    return f"{instance.name}/preview.{extension}"
+    return f"{instance.original.name}/preview.{extension}"
 
 
 class Preview(models.Model):
-    name = models.CharField(primary_key=True, max_length=100)
     file = models.ImageField(upload_to=preview_upload_path)
     original = models.ForeignKey(Original, on_delete=models.CASCADE)
 
 
 def tile_upload_path(instance, filename):
     extension = filename.split('.')[-1]
-    return f"{instance.name}/scale_{instance.scale}/t_{instance.x}_{instance.y}.{extension}"
+    return f"{instance.original.name}/scale_{instance.scale}/t_{instance.x}_{instance.y}.{extension}"
 
 
 class ScaleTile(models.Model):
